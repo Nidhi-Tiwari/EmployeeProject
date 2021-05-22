59324c0cedd2966e60477873b4f6da25a7abc022
Nidhi <nidhitiwari2696@gmail.com>
2021-05-22 18:18:29 +0530

    changes

--- employee/src/main/java/com/prepare/employee/Controllers/EmployeeController.java
+++ employee/src/main/java/com/prepare/employee/Controllers/EmployeeController.java
@@ -1,34 +1,54 @@
 package com.prepare.employee.Controllers;
 
-import com.prepare.employee.DataBaseConnection.Employee;
+import com.prepare.employee.DataBaseConnection.EmployeeEntity;
 import com.prepare.employee.Exceptions.CustomExceptions;
 import com.prepare.employee.Repositories.EmployeeRepository;
+import com.prepare.employee.Services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping(value = {"/register"} , method = RequestMethod.POST,produces = "application/json")
-    public Employee registerEmployee(@RequestBody Employee emp){
-        return this.employeeRepository.save(emp);
+    @RequestMapping(value = {"/register"}, method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> registerEmployee(@RequestBody Map<String, Object> requestBody) {
+        if (!requestBody.containsKey("departmentId"))
+            return new ResponseEntity<>("Department id is missing !!", HttpStatus.BAD_REQUEST);
+        if (!requestBody.containsKey("name"))
+            return new ResponseEntity<>("Name is missing !!", HttpStatus.BAD_REQUEST);
+        if (!requestBody.containsKey("salary"))
+            return new ResponseEntity<>("Salary is missing !!", HttpStatus.BAD_REQUEST);
+        int managerId = 0;
+        String address = "default";
+        if (requestBody.containsKey("managerId"))
+            managerId = (int) requestBody.get("managerId");
+        if (requestBody.containsKey("address"))
+            address = requestBody.get("address").toString();
+        int departmentId = (int) requestBody.get("departmentId");
+        int salary = (int) requestBody.get("salary");
+        String name = requestBody.get("name").toString();
+        ResponseEntity<?> responseEntity = EmployeeService.getInstance().registerUser(name, departmentId, salary, managerId, address);
+        return responseEntity;
     }
 
-    @RequestMapping(value = {"/deleteUser"},produces = "application/json",method = RequestMethod.DELETE)
-    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id){
-        Employee emp = this.employeeRepository.findById(id).
-                orElseThrow(()-> new CustomExceptions("User not found with user id "+id));
+    @RequestMapping(value = {"/deleteUser"}, produces = "application/json", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteEmployee(@RequestParam Integer id) {
+        EmployeeEntity emp = this.employeeRepository.findById(id).
+                orElseThrow(() -> new CustomExceptions("User not found with user id " + id));
         this.employeeRepository.delete(emp);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>("User Deleted Successfully",HttpStatus.OK);
     }
 
-    @RequestMapping(value = {"/getEmployee"}, produces ="application/json",method = RequestMethod.GET)
-    public Employee getEmployeeDetails(@PathVariable Long id){
-        Employee emp = this.employeeRepository.findById(id).orElseThrow(()-> new CustomExceptions("User not found with user id "+id));
+    @RequestMapping(value = {"/getEmployee"}, produces = "application/json", method = RequestMethod.GET)
+    public EmployeeEntity getEmployeeDetails(@RequestParam Integer id) {
+        EmployeeEntity emp = this.employeeRepository.findById(id).orElseThrow(() -> new CustomExceptions("User not found with user id " + id));
         return emp;
     }
 }
--- employee/src/main/java/com/prepare/employee/Repositories/EmployeeRepository.java
+++ employee/src/main/java/com/prepare/employee/Repositories/EmployeeRepository.java
@@ -1,9 +1,9 @@
 package com.prepare.employee.Repositories;
 
-import com.prepare.employee.DataBaseConnection.Employee;
+import com.prepare.employee.DataBaseConnection.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer> {
 }

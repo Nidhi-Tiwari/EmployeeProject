b11513fd774cf0034f1ae9e4f2a4fd15abecb475
Nidhi <nidhitiwari2696@gmail.com>
2021-06-06 11:28:24 +0530

    new changes 1

--- employee/src/main/java/com/prepare/employee/Services/impl/UpdateEmployeeDetailsServiceImpl.java
+++ employee/src/main/java/com/prepare/employee/Services/impl/UpdateEmployeeDetailsServiceImpl.java
@@ -6,23 +6,22 @@ import com.prepare.employee.DataBaseConnection.EmployeeEntity;
 import com.prepare.employee.Exceptions.CustomExceptions;
 import com.prepare.employee.Repositories.EmployeeRepository;
 import com.prepare.employee.Services.UpdateEmployeeDetailsService;
-import com.prepare.employee.util.CustomMapper;
 import com.prepare.employee.util.RequestParamValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Locale;
+import org.springframework.util.StringUtils;
+import org.apache.logging.log4j.util.Strings;
 
 @Service
 public class UpdateEmployeeDetailsServiceImpl implements UpdateEmployeeDetailsService {
     private final RequestParamValidator<UpdateRequest> updateRequestRequestParamValidator;
     private final EmployeeRepository employeeRepository;
-    private final CustomMapper customMapper;
 
-    public UpdateEmployeeDetailsServiceImpl(RequestParamValidator<UpdateRequest> updateRequestRequestParamValidator, EmployeeRepository employeeRepository, CustomMapper customMapper) {
+    public UpdateEmployeeDetailsServiceImpl(RequestParamValidator<UpdateRequest> updateRequestRequestParamValidator,
+                                            EmployeeRepository employeeRepository) {
         this.updateRequestRequestParamValidator = updateRequestRequestParamValidator;
         this.employeeRepository = employeeRepository;
-        this.customMapper = customMapper;
+//        this.customMapper = customMapper;
     }
 
     @Transactional
@@ -31,8 +30,14 @@ public class UpdateEmployeeDetailsServiceImpl implements UpdateEmployeeDetailsSe
         updateRequestRequestParamValidator.validate(updateRequest);
         EmployeeEntity emp = this.employeeRepository.findById(updateRequest.getId()).
                 orElseThrow(() -> new CustomExceptions("User not found with user id " + updateRequest.getId()));
-        customMapper.updateEmployeeFromEmployeeTable(updateRequest, emp);
+        emp.setSalary(updateRequest.getSalary() != null ? updateRequest.getSalary() : emp.getSalary());
+        emp.setName(Strings.isBlank(updateRequest.getName()) ? emp.getName() : updateRequest.getName());
+        emp.setDepartmentId(updateRequest.getDepartmentId() != null ? updateRequest.getDepartmentId() : emp.getDepartmentId());
+        emp.setManagerId(updateRequest.getManagerId() != null ? updateRequest.getManagerId() : emp.getManagerId());
+        emp.setAddress(Strings.isBlank(updateRequest.getAddress()) ? emp.getAddress() : updateRequest.getAddress());
+
         EmployeeEntity employeeEntityResponse = this.employeeRepository.save(emp);
+
         return EmployeeDetailsResponse.builder().address(employeeEntityResponse.getAddress())
                 .departmentId(employeeEntityResponse.getDepartmentId()).
                         id(employeeEntityResponse.getId()).

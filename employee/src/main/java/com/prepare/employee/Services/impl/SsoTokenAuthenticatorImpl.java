496856d2b0cc474106bda63a8e59a326173d9870
Prakhar Tripathi <prakhar.tripathi@paytm.com>
2021-06-09 10:04:11 +0530

    Hystrix circuit breaker and properties

--- employee/src/main/java/com/prepare/employee/Services/impl/SsoTokenAuthenticatorImpl.java
+++ employee/src/main/java/com/prepare/employee/Services/impl/SsoTokenAuthenticatorImpl.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.prepare.employee.Services.SsoTokenAuthenticator;
 import com.prepare.employee.domain.response.Token;
+import com.prepare.employee.util.ExternalHitHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,12 +20,16 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @Slf4j
 public class SsoTokenAuthenticatorImpl implements SsoTokenAuthenticator {
+    @Autowired
+    private ExternalHitHelper externalHitHelper;
+
     @Override
     public boolean authenticate(String ssoToken) {
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            Token response = getTokenHit(ssoToken);
+            if(Strings.isBlank(ssoToken)) {
+                return false;
+            }
+            Token response = externalHitHelper.getTokenHit(ssoToken);
             log.info("Response name ={} " , response.getName());
             log.info("Response userId ={} " , response.getUserId());
             if (ssoToken.equals("usr-123_9"))
@@ -33,18 +40,6 @@ public class SsoTokenAuthenticatorImpl implements SsoTokenAuthenticator {
             return false;
         }
     }
-    @HystrixCommand(fallbackMethod = "getTokenHitFallBack")
-    public Token getTokenHit(String ssoToken) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        Token response = restTemplate.exchange("http://localhost:8082/oauth/token?sso=" + ssoToken,
-                HttpMethod.GET, new HttpEntity<>(headers), Token.class).getBody();
-        return response;
-    }
 
-    public Token getTokenHitFallBack(String ssoToken) {
 
-        log.info("Default response for getToken Hit");
-        return Token.builder().name("Default").userId(0).build();
-    }
 }

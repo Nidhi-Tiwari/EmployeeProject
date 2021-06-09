0101724a58c862a4600850eb065baa34d20b4225
Prakhar Tripathi <prakhar.tripathi@paytm.com>
2021-06-09 09:13:07 +0530

    Circuit breaker

--- employee/src/main/java/com/prepare/employee/Services/impl/SsoTokenAuthenticatorImpl.java
+++ employee/src/main/java/com/prepare/employee/Services/impl/SsoTokenAuthenticatorImpl.java
@@ -2,6 +2,7 @@ package com.prepare.employee.Services.impl;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.prepare.employee.Services.SsoTokenAuthenticator;
 import com.prepare.employee.domain.response.Token;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +22,7 @@ public class SsoTokenAuthenticatorImpl implements SsoTokenAuthenticator {
         try {
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
-            Token response = restTemplate.exchange("http://localhost:8082/oauth/token?sso=" + ssoToken,
-                    HttpMethod.GET, new HttpEntity<>(headers), Token.class).getBody();
+            Token response = getTokenHit(ssoToken);
             log.info("Response name ={} " , response.getName());
             log.info("Response userId ={} " , response.getUserId());
             if (ssoToken.equals("usr-123_9"))
@@ -32,5 +32,19 @@ public class SsoTokenAuthenticatorImpl implements SsoTokenAuthenticator {
             log.error("Not able to authenticate");
             return false;
         }
+    }
+    @HystrixCommand(fallbackMethod = "getTokenHitFallBack")
+    public Token getTokenHit(String ssoToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        Token response = restTemplate.exchange("http://localhost:8082/oauth/token?sso=" + ssoToken,
+                HttpMethod.GET, new HttpEntity<>(headers), Token.class).getBody();
+        return response;
+    }
+
+    public Token getTokenHitFallBack(String ssoToken) {
+
+        log.info("Default response for getToken Hit");
+        return Token.builder().name("Default").userId(0).build();
     }
 }
